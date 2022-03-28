710b2543e9419708f1d9a8fb1512584e99e931f7
Marsel151205 <mambetalievmarsel2289@gmail.com>
2022-03-28 23:27:54 +0600

    GITHUB

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,43 +4,39 @@ import java.util.Random;
 
 public class Main {
 
-    public static int[] heroesHealth = {270,280,260, 500}; //воин, маг и кинетик
-    public static int[] heroesDamage = {20,15,25, 0};
-    public static String[] heroesAttackType = {"Physical","Magical","Kinetic", "Medic"};
+    public static int[] heroesHealth = {270, 280, 190, 300}; //воин, маг и кинетик
+    public static int[] heroesDamage = {20, 15, 25, 0};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medic"};
 
     public static int medicHelp = 25;
     public static int indexMedic = 3;
 
-
-    public static int bossHealth = 900;
-    public static  int bossDamage = 50;
-    public static String bossDefenceType = "";
+    public static int bossHealth = 720;
+    public static int bossDamage = 50;
+    public static String bossDefenceType;
     public static int roundNumber = 0;
 
-
-
-
-
     public static void medic() {
-        for (int i = 0; i < heroesHealth.length; i++){
+        for (int i = 0; i < heroesHealth.length; i++) {
             if (heroesHealth[i] < 100 && heroesHealth[i] > 0 && heroesHealth[i] != heroesHealth[indexMedic]) {
-               Random random = new Random();
-               int randomMedic = random.nextInt(heroesHealth.length - 1);
-               heroesHealth[i] = randomMedic + medicHelp;
-                break;
+                Random random = new Random();
+                int randomMedic = random.nextInt(heroesHealth.length);
+                if (heroesHealth[randomMedic] != heroesHealth[indexMedic]) {
+                    heroesHealth[randomMedic] = heroesHealth[randomMedic] + medicHelp;
+                    System.out.println(heroesHealth[randomMedic] + heroesAttackType[randomMedic]);
+                    break;
+                }
             }
         }
-}
-
+    }
 
     public static void main(String[] args) {
         printDStatistics();
         isGameFinished();
-        while (!isGameFinished()){
+        while (!isGameFinished()) {
             round();
             medic();
         }
-
     }
 
     private static void round() {
@@ -48,64 +44,63 @@ public class Main {
         chooseBossDefenceType();
         bossHits();
         heroesHit();
-        medic();
         printDStatistics();
     }
 
-    public static void chooseBossDefenceType(){
+    public static void chooseBossDefenceType() {
         Random random = new Random();
         int randNum = random.nextInt(heroesAttackType.length);  // 0 1 2
         bossDefenceType = heroesAttackType[randNum];
-        System.out.println("Boss choose: "+ bossDefenceType);
+        System.out.println("Boss choose: " + bossDefenceType);
     }
 
-    public static void bossHits(){
+    public static void bossHits() {
         for (int i = 0; i < heroesHealth.length; i++) {
-            if(heroesHealth[i] < bossDamage){
+            if (heroesHealth[i] < bossDamage) {
                 heroesHealth[i] = 0;
-            } else{
-                heroesHealth[i] =  heroesHealth[i] - bossDamage;
+            } else {
+                heroesHealth[i] = heroesHealth[i] - bossDamage;
             }
         }
     }
 
-    public static void heroesHit(){
+    public static void heroesHit() {
         for (int i = 0; i < heroesDamage.length; i++) {
-            if(heroesHealth[i]>0 && bossHealth>0){
-                if(heroesAttackType[i] == bossDefenceType){
+            if (heroesHealth[i] > 0 && bossHealth > 0) {
+                if (heroesAttackType[i] == bossDefenceType) {
                     Random r = new Random();
-                    int coefficient = r.nextInt(8)+2; // 0 1 2 3 4 5 6 7
+                    int coefficient = r.nextInt(8) + 2;
 
-                    if(bossHealth < heroesDamage[i]*coefficient){
+                    if (bossHealth < heroesDamage[i] * coefficient) {
                         bossHealth = 0;
                     } else {
-                        bossHealth =  bossHealth - heroesDamage[i]*coefficient;
+                        bossHealth = bossHealth - heroesDamage[i] * coefficient;
                     }
-                    System.out.println("Critical Damage: "+ heroesDamage[i]*coefficient);
+                    System.out.println("Critical Damage: " + heroesDamage[i] * coefficient);
                 } else {
-                    if(bossHealth < heroesDamage[i]){
+                    if (bossHealth < heroesDamage[i]) {
                         bossHealth = 0;
                     } else {
-                        bossHealth =  bossHealth - heroesDamage[i];
+                        bossHealth = bossHealth - heroesDamage[i];
                     }
                 }
             }
         }
     }
 
-    public static Boolean isGameFinished(){
-        if(bossHealth <= 0){
+    public static Boolean isGameFinished() {
+        if (bossHealth <= 0) {
             System.out.println("Heroes won!");
             return true;
         }
         boolean allHeroesDead = true;
-        for (int i = 0; i < heroesHealth.length; i++) {
-            if(heroesHealth[i]>0){
+        for (int j : heroesHealth) {
+            if (j > 0) {
                 allHeroesDead = false;
                 break;
             }
         }
-        if (allHeroesDead){
+        if (allHeroesDead) {
             System.out.println("BOSS WON!");
         }
         return allHeroesDead;
@@ -113,9 +108,9 @@ public class Main {
 
     public static void printDStatistics() {
         System.out.println("----" + roundNumber + " ROUND----");
-        System.out.println("Boss health: "+ bossHealth + " [" + bossDamage + "]" );
-        for (int i = 0; i < heroesHealth.length ; i++) {
-            System.out.println(heroesAttackType[i] + " health: " + heroesHealth[i] + " [" + heroesDamage[i] + "]" );
+        System.out.println("Boss health: " + bossHealth + " [" + bossDamage + "]");
+        for (int i = 0; i < heroesHealth.length; i++) {
+            System.out.println(heroesAttackType[i] + " health: " + heroesHealth[i] + " [" + heroesDamage[i] + "]");
         }
     }
 }
